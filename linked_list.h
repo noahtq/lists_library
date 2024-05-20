4f0044a9f158f11423ef126e96ddf13755caae0b
noahtq <vfx.noaht@gmail.com>
2024-05-20 14:03:18 -0700

    Finish changing linkedlist class to a template, still need to test move constructor for template

--- linked_list.h
+++ linked_list.h
@@ -5,76 +5,166 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
-#include <string>
-
 namespace Lists {
-    struct Data {
-        int ranking;
-        std::string first_name;
-        std::string last_name;
-        std::string nationality;
-    };
 
-    bool operator==(const Data& a, const Data& b);
-    bool operator!=(const Data& a, const Data& b);
-
-    class Node {
+    template <typename T> class Node {
     private:
-        Data data;
+        T data;
         Node* next;
         Node* prev;
     public:
         //Constructors
-        Node(Data new_data);
+        explicit Node(T new_data);
 
         //Getters and setters
         [[nodiscard]] Node* get_next() const {return next;}
         [[nodiscard]] Node* get_prev() const {return prev;}
-        [[nodiscard]] Data get_data() const {return data;}
+        [[nodiscard]] T get_data() const {return data;}
 
         void set_next(Node* new_next) {next = new_next;}
         void set_prev(Node* new_prev) {prev = new_prev;}
-        void set_data(Data new_data) {data = new_data;}
+        void set_data(T& new_data) {data = new_data;}
     };
 
-    inline bool operator==(const Node& a, const Node& b) {return a.get_data() == b.get_data();}
-    inline bool operator!=(const Node& a, const Node& b) {return a.get_data() != b.get_data();}
+    template<typename T>
+    Node<T>::Node(T new_data)
+    :data(new_data), next(nullptr), prev(nullptr) {}
 
-    class LinkedList {
+    template<typename T>
+    bool operator==(const Node<T>& a, const Node<T>& b) {return a.get_data() == b.get_data();}
+    template<typename T>
+    bool operator!=(const Node<T>& a, const Node<T>& b) {return a.get_data() != b.get_data();}
+
+    template <typename T> class LinkedList {
     private:
-        Node* head = nullptr;
-        Node* tail = nullptr;
+        Node<T>* head = nullptr;
+        Node<T>* tail = nullptr;
         int sz = 0;
     public:
         //Constructors
         LinkedList() = default;
 
-        //Copy constructor and operator
-        LinkedList(const LinkedList& list);
-        LinkedList& operator=(const LinkedList& list);
+        // Copy constructor and operator
+         LinkedList(const LinkedList& list);
+         LinkedList& operator=(const LinkedList& list);
 
         //Move Constructor
         LinkedList(LinkedList&& list) noexcept;
 
         //Getters and setters
         [[nodiscard]] int size() const {return sz;}
-        [[nodiscard]] Node* get_head() const {return head;}
-        [[nodiscard]] Node* get_tail() const {return tail;}
+        [[nodiscard]] Node<T>* get_head() const {return head;}
+        [[nodiscard]] Node<T>* get_tail() const {return tail;}
 
         //Methods
-        void appendNode(Data new_data);
+        void appendNode(T new_data);
         void emptyList();
 
         //Destructor
         ~LinkedList() {emptyList();}
     };
 
-    bool operator==(const LinkedList& a, const LinkedList& b);
-    bool operator!=(const LinkedList& a, const LinkedList& b);
-    inline bool operator<(const LinkedList& a, const LinkedList& b) {return a.size() < b.size();}
-    inline bool operator<=(const LinkedList& a, const LinkedList& b) {return a.size() <= b.size();}
-    inline bool operator>(const LinkedList& a, const LinkedList& b) {return a.size() > b.size();}
-    inline bool operator>=(const LinkedList& a, const LinkedList& b) {return a.size() >= b.size();}
+    //Copy constructor
+    template <typename T>
+    LinkedList<T>::LinkedList(const LinkedList &list) {
+        Node<T>* walker = list.get_head();
+        while (walker != nullptr) {
+            this->appendNode(walker->get_data());
+            walker = walker->get_next();
+        }
+    }
+
+    // //Copy operator for LinkedList class
+    //Make copies of all nodes in linked list and add those copies to the new list
+    template <typename T>
+    LinkedList<T>& LinkedList<T>::operator=(const LinkedList& list) {
+        //TODO: once equality operators have been implemented, update this function to check and make sure that no operation is performed if we're assigning the list to itself
+        Node<T>* walker = list.get_head();
+        while (walker != nullptr) {
+            this->appendNode(walker->get_data());
+            walker = walker->get_next();
+        }
+        return *this;
+    }
+
+    // //Move constructor
+    template <typename T>
+    LinkedList<T>::LinkedList(LinkedList &&list) noexcept
+    :head(list.get_head()), tail(list.get_tail()), sz(list.size()) {
+        list.sz = 0;
+        list.head = nullptr;
+        list.tail = nullptr;
+    }
+
+    template <typename T>
+    void LinkedList<T>::appendNode(T new_data) {
+        Node<T>* new_node = new Node(new_data);
+
+        if (!head) {
+            head = new_node;
+            tail = new_node;
+        } else {
+            new_node->set_prev(tail);
+            tail->set_next(new_node);
+            tail = new_node;
+        }
+        sz++;
+    }
+
+    template <typename T>
+    void LinkedList<T>::emptyList() {
+        Node<T>* walker = head;
+        while (walker != nullptr) {
+            Node<T>* link = walker->get_next();
+            delete walker;
+            walker = link;
+        }
+        head = nullptr;
+        tail = nullptr;
+        sz = 0;
+    }
+
+    template <typename T>
+    bool operator==(const LinkedList<T>& a, const LinkedList<T>& b) {
+        if (a.size() != b.size()) {
+            return false;
+        }
+        Node<T>* a_walker = a.get_head();
+        Node<T>* b_walker = b.get_head();
+        while (a_walker != nullptr && b_walker != nullptr) {
+            if (!(*a_walker == *b_walker)) return false;
+            a_walker = a_walker->get_next();
+            b_walker = b_walker->get_next();
+        }
+        return true;
+    }
+
+    template <typename T>
+    bool operator!=(const LinkedList<T>& a, const LinkedList<T>& b) {
+        if (a.size() != b.size()) {
+            return true;
+        }
+        Node<T>* a_walker = a.get_head();
+        Node<T>* b_walker = b.get_head();
+        while (a_walker != nullptr && b_walker != nullptr) {
+            if (!(*a_walker == *b_walker)) return true;
+            a_walker = a_walker->get_next();
+            b_walker = b_walker->get_next();
+        }
+        return false;
+    }
+
+    template <typename T>
+    bool operator<(const LinkedList<T>& a, const LinkedList<T>& b) {return a.size() < b.size();}
+
+    template <typename T>
+    bool operator<=(const LinkedList<T>& a, const LinkedList<T>& b) {return a.size() <= b.size();}
+
+    template <typename T>
+    bool operator>(const LinkedList<T>& a, const LinkedList<T>& b) {return a.size() > b.size();}
+
+    template <typename T>
+    bool operator>=(const LinkedList<T>& a, const LinkedList<T>& b) {return a.size() >= b.size();}
 
 }
 
--- main.cpp
+++ main.cpp
@@ -1,64 +1,67 @@
 #include <iostream>
-#include <vector>
+
 #include "linked_list.h"
 
 using namespace Lists;
 
-template<typename T, int i>
-class Vector {
-private:
-    T arr[i];
-    T* initialized = arr;
-public:
-    Vector() = default;
-    void pushBack(T elem) {
-        if (initialized < arr + i) {
-            *initialized = elem;
-            ++initialized;
-        } else {
-            throw std::out_of_range("Vector full");
-        }
-    }
+struct Golfer {
+    int ranking;
+    std::string first_name;
+    std::string last_name;
+    std::string nationality;
 };
 
+bool operator==(const Golfer& a, const Golfer& b) {
+    if (a.ranking != b.ranking) return false;
+    if (a.first_name != b.first_name) return false;
+    if (a.last_name != b.last_name) return false;
+    if (a.nationality != b.nationality) return false;
+    return true;
+}
+
+bool operator!=(const Golfer& a, const Golfer& b) {
+    if (a.ranking != b.ranking) return true;
+    if (a.first_name != b.first_name) return true;
+    if (a.last_name != b.last_name) return true;
+    if (a.nationality != b.nationality) return true;
+    return false;
+}
+
 int main() {
-    Vector<double, 5> dubs;
-    dubs.pushBack(3.2);
-    dubs.pushBack(2.1);
 
-    Vector<char, 2> chars;
-    chars.pushBack('A');
-    chars.pushBack('B');
+    Golfer sheffler = {1, "Scottie", "Sheffler", "American"};
+    Golfer fleetwood = {11, "Tommy", "Fleetwood", "Englist"};
+    Golfer rahm = {3, "John", "Rahm", "Spanish"};
 
-    chars.pushBack('C');
+    LinkedList<Golfer> list;
+    list.appendNode(sheffler);
+    list.appendNode(fleetwood);
+    list.appendNode(rahm);
 
+    LinkedList<Golfer> list2;
+    list2.appendNode(sheffler);
+    list2.appendNode(rahm);
 
-    Data sheffler = {1, "Scottie", "Sheffler", "American"};
-    Data fleetwood = {11, "Tommy", "Fleetwood", "Englist"};
-    Data rahm = {3, "John", "Rahm", "Spanish"};
+    std::cout << (list == list2) << std::endl;
 
-    LinkedList golfers;
-    golfers.appendNode(sheffler);
-    golfers.appendNode(fleetwood);
-    golfers.appendNode(rahm);
+    LinkedList<int> int_list;
+    int_list.appendNode(2);
+    int_list.appendNode(3);
+    int_list.appendNode(4);
 
-    LinkedList golfers2 = std::move(golfers);
-    LinkedList golfer_copy;
-    golfer_copy = golfers2;
-    LinkedList golfer_copy2 = golfers2;
-    golfer_copy2.appendNode(fleetwood);
-    LinkedList golfers3;
-    golfers3.appendNode(sheffler);
-    golfers3.appendNode(sheffler);
-    golfers3.appendNode(sheffler);
+    LinkedList<int> int_list2;
+    int_list2.appendNode(2);
+    int_list2.appendNode(3);
+    int_list2.appendNode(3);
 
-    std::cout << (golfers2 == golfer_copy) << std::endl;
-    std::cout << (golfers2 == golfer_copy2) << std::endl;
-    std::cout << (golfers2 == golfers3) << std::endl;
+    std::cout << (int_list == int_list2) << std::endl;
 
-    std::cout << (golfers2 != golfer_copy) << std::endl;
-    std::cout << (golfers2 != golfer_copy2) << std::endl;
-    std::cout << (golfers2 != golfers3) << std::endl;
+    LinkedList<int> int_list3 = int_list;
+    LinkedList<int> int_list4;
+    int_list4 = int_list;
+
+    LinkedList<int> int_list5;
+
 
     return 0;
 }
