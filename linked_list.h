f80ed0829bf2c35a570fa11bf09a7ae6eb026936
noahtq <vfx.noaht@gmail.com>
2024-05-21 13:54:51 -0500

    Add output stream insertion operator for linkedList and added node insertion methods for linkedlist template

--- linked_list.h
+++ linked_list.h
@@ -5,6 +5,9 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
+#include <iostream>
+#include <stdexcept>
+
 namespace Lists {
 
     template <typename T> class Node {
@@ -12,6 +15,7 @@ namespace Lists {
         T data;
         Node* next;
         Node* prev;
+
     public:
         //Constructors
         explicit Node(T new_data);
@@ -27,10 +31,6 @@ namespace Lists {
     };
 
     template<typename T>
-    Node<T>::Node(T new_data)
-    :data(new_data), next(nullptr), prev(nullptr) {}
-
-    template<typename T>
     bool operator==(const Node<T>& a, const Node<T>& b) {return a.get_data() == b.get_data();}
     template<typename T>
     bool operator!=(const Node<T>& a, const Node<T>& b) {return a.get_data() != b.get_data();}
@@ -40,6 +40,10 @@ namespace Lists {
         Node<T>* head = nullptr;
         Node<T>* tail = nullptr;
         int sz = 0;
+
+        //Private helper functions
+        void helperInsert(Node<T>* prev_node, T new_data);
+
     public:
         //Constructors
         LinkedList() = default;
@@ -57,12 +61,23 @@ namespace Lists {
         [[nodiscard]] Node<T>* get_tail() const {return tail;}
 
         //Methods
+        bool nodeInList(const Node<T>* target_node);
+        Node<T>* findNode(const T& target_data);
         void appendNode(T new_data);
+        void insertNode(T new_data);
+        void insertNode(Node<T>* prev_node, T new_data);
+        void insertNodeBeginning(T new_data);
         void emptyList();
 
         //Destructor
         ~LinkedList() {emptyList();}
     };
+
+    /* ------------------------------------------------------------------------ */
+
+    template<typename T>
+    Node<T>::Node(T new_data)
+    :data(new_data), next(nullptr), prev(nullptr) {}
 
     //Copy constructor
     template <typename T>
@@ -74,7 +89,7 @@ namespace Lists {
         }
     }
 
-    // //Copy operator for LinkedList class
+    //Copy operator for LinkedList class
     //Make copies of all nodes in linked list and add those copies to the new list
     template <typename T>
     LinkedList<T>& LinkedList<T>::operator=(const LinkedList& list) {
@@ -88,13 +103,48 @@ namespace Lists {
         return *this;
     }
 
-    // //Move constructor
+    //Move constructor
     template <typename T>
     LinkedList<T>::LinkedList(LinkedList &&list) noexcept
     :head(list.get_head()), tail(list.get_tail()), sz(list.size()) {
         list.sz = 0;
         list.head = nullptr;
         list.tail = nullptr;
+    }
+
+    template <typename T>
+    bool LinkedList<T>::nodeInList(const Node<T>* target_node) {
+        Node<T>* walker = head;
+        while (walker != nullptr) {
+            if (walker == target_node) {
+                return true;
+            }
+            walker = walker->get_next();
+        }
+        return false;
+    }
+
+    //Assumes the datatype can be printed
+    template <typename T>
+    std::ostream& operator<<(std::ostream& os, const LinkedList<T>& list) {
+        Node<T>* walker = list.get_head();
+        while (walker != nullptr) {
+            os << walker->get_data() << ' ';
+            walker = walker->get_next();
+        }
+        return os;
+    }
+
+    template <typename T>
+    Node<T>* LinkedList<T>::findNode(const T& target_data) {
+        Node<T>* walker = head;
+        while (walker != nullptr) {
+            if (walker->get_data() == target_data) {
+                return walker;
+            }
+            walker = walker->get_next();
+        }
+        return nullptr;
     }
 
     template <typename T>
@@ -110,6 +160,56 @@ namespace Lists {
             tail = new_node;
         }
         sz++;
+    }
+
+    template<typename T>
+    void LinkedList<T>::helperInsert(Node<T>* prev_node, T new_data) {
+        if (sz < 1) {
+            throw std::out_of_range("List is empty, can't insert node. Try using .appendNode() instead.");
+        }
+        Node<T>* new_node = new Node(new_data);
+
+        if (prev_node == nullptr) { //Beginning of list
+            Node<T>* next_node = head;
+            new_node->set_next(next_node);
+            new_node->set_prev(nullptr);
+            next_node->set_prev(new_node);
+            head = new_node;
+        } else if (prev_node->get_next() == nullptr) { //End of list
+            new_node->set_next(nullptr);
+            new_node->set_prev(prev_node);
+            prev_node->set_next(new_node);
+            tail = new_node;
+        } else { //Somewhere other than beginning or end
+            Node<T>* next_node = prev_node->get_next();
+            new_node->set_next(next_node);
+            new_node->set_prev(prev_node);
+            prev_node->set_next(new_node);
+            next_node->set_prev(new_node);
+        }
+    }
+
+    //Insertion with no previous node specified will be considered an append
+    template<typename T>
+    void LinkedList<T>::insertNode(T new_data) {
+        this->appendNode(new_data);
+    }
+
+    template<typename T>
+    void LinkedList<T>::insertNode(Node<T>* prev_node, T new_data) {
+        if (sz < 1) {
+            throw std::out_of_range("List is empty, can't insert node. Try using .appendNode() instead.");
+        }
+        if (nodeInList(prev_node)) {
+            helperInsert(prev_node, new_data);
+        } else {
+            throw std::out_of_range("Previous node could not be found in list");
+        }
+    }
+
+    template<typename T>
+    void LinkedList<T>::insertNodeBeginning(T new_data) {
+        helperInsert(nullptr, new_data);
     }
 
     template <typename T>
